cd2466f0aa44f9c97c052525e88a1d2302644151
Omaritto <omaribaf@gmail.com>
2024-06-04 22:39:46 +0200

    Update Camera2D.cs

--- scripts/Camera2D.cs
+++ scripts/Camera2D.cs
@@ -3,7 +3,7 @@ namespace LebJam.scripts;
 public partial class Camera2D : Godot.Camera2D
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public override void _Ready( )
 	{
 	}
 
