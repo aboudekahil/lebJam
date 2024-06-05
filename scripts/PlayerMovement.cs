186cc49b4a053e18525f85c70f77b2c2880ff2f4
Smaugchamp <smaugchamp@gmail.com>
2024-06-05 10:43:07 +0200

    move player

--- scripts/PlayerMovement.cs
+++ scripts/PlayerMovement.cs
@@ -4,68 +4,68 @@ namespace LebJam.scripts;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-    private const float NormalSpeed = 85.0f;
+	private const float NormalSpeed = 85.0f;
 
-    private const float RunningSpeedBoost = 100.0f;
-    private float _baseRotation;
-    private float _rotation;
+	private const float RunningSpeedBoost = 100.0f;
+	private float _baseRotation;
+	private float _rotation;
 
-    public override void _Ready()
-    {
-        _baseRotation = RotationDegrees;
-        _rotation = _baseRotation;
-    }
+	public override void _Ready()
+	{
+		_baseRotation = RotationDegrees;
+		_rotation = _baseRotation;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        MovePlayer(delta);
-        RotateTowardsMouse(delta);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		MovePlayer(delta);
+		RotateTowardsMouse(delta);
+	}
 
-    private void MovePlayer(double delta)
-    {
-        var isRunning = Input.IsActionPressed("running");
-        var speed = NormalSpeed + (isRunning ? RunningSpeedBoost : 0.0f);
+	private void MovePlayer(double delta)
+	{
+		var isRunning = Input.IsActionPressed("running");
+		var speed = NormalSpeed + (isRunning ? RunningSpeedBoost : 0.0f);
 
-        var velocity = Velocity;
+		var velocity = Velocity;
 
-        var direction =
-            Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        if (direction != Vector2.Zero)
-        {
-            velocity = direction.Normalized() * speed;
-        } else
-        {
-            velocity = velocity.MoveToward(Vector2.Zero,
-                (float)delta * speed * 11f);
-        }
+		var direction =
+			Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction.Normalized() * speed;
+		} else
+		{
+			velocity = velocity.MoveToward(Vector2.Zero,
+				(float)delta * speed * 11f);
+		}
 
 
-        Velocity = velocity;
-        MoveAndSlide();
-        
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+		
+	}
 
-    private void RotateTowardsMouse(double delta)
-    {
-        var mousePosition = GetGlobalMousePosition();
-        var position = GlobalPosition;
+	private void RotateTowardsMouse(double delta)
+	{
+		var mousePosition = GetGlobalMousePosition();
+		var position = GlobalPosition;
 
-        float newRotation;
-        
-        if (position.X < mousePosition.X)
-        {
-            newRotation = -Mathf.Atan2(position.X - mousePosition.X,
-                position.Y - mousePosition.Y) - Mathf.Pi / 2;
-        }
-        else
-        {
-            newRotation = -Mathf.Atan2(mousePosition.X - position.X,
-                mousePosition.Y - position.Y) - Mathf.Pi / 2;
-        }
+		float newRotation;
+		
+		if (position.X < mousePosition.X)
+		{
+			newRotation = -Mathf.Atan2(position.X - mousePosition.X,
+				position.Y - mousePosition.Y) - Mathf.Pi / 2;
+		}
+		else
+		{
+			newRotation = -Mathf.Atan2(mousePosition.X - position.X,
+				mousePosition.Y - position.Y) - Mathf.Pi / 2;
+		}
 
-        newRotation = (float) Mathf.Clamp(newRotation, -0.55, 0.55);
+		newRotation = (float) Mathf.Clamp(newRotation, -0.55, 0.55);
 
-        Rotation = Mathf.RotateToward(Rotation, newRotation, (float) delta * 10);
-    }
+		Rotation = Mathf.RotateToward(Rotation, newRotation, (float) delta * 10);
+	}
 }
