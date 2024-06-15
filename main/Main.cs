78e9bd5f2b500786a618fd826a1c281e9286cb0f
plutonoire <kachmarhaydi0@gmail.com>
2024-06-15 22:40:50 +0300

    tree
    tree

--- main/Main.cs
+++ main/Main.cs
@@ -4,11 +4,11 @@ namespace LebJam.main;
 
 public partial class Main : Node2D
 {
-    public override void _Input(InputEvent @event)
-    {
-        if (@event.IsActionPressed("ui_cancel"))
-        {
-            GetTree().Quit();
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_cancel"))
+		{
+			GetTree().Quit();
+		}
+	}
 }
